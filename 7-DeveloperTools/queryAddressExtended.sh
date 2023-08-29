04d9b98f62b77c9fc17e85493f9496d05dc7df14
Vortecs <vortecsmaster@vortecsla.com>
2023-08-29 09:54:25 -0500

    Updates
    Some updates on the scripts for Blockfrost

--- 7-DeveloperTools/queryAddressExtended.sh
+++ 7-DeveloperTools/queryAddressExtended.sh
@@ -1,4 +1,4 @@
-curl -H 'project_id: previewTQgzYm9vxX3Ot5sLA7m1YIpa5ckZNERq' https://cardano-preview.blockfrost.io/api/v0/addresses/$Adr01/extended | jq
+curl -H 'project_id: previewTQgzYm9vxX3Ot5sLA7m1YIpa5ckZNERq' https://cardano-preview.blockfrost.io/api/v0/addresses/$nami/extended | jq
 
 
 
